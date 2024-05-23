fbed6791c842b052c7b97b00c5ae8d6bff561bbe
Shirley_Grace <giray.mungai@gmail.com>
2024-05-23 12:05:29 +0300

    about us

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -10,29 +10,33 @@ const About = () => {
     <div className="middle">
         <h2>What is Curious Minds</h2>
         <p>
-            We are a platform that enebles the groeth of your 
-            child without chash hriwod hgoenwp wue ur tt ei e i <br />
-            hfirfiekedncrur erhfekjnfkjnej ei i beiiev thwt it can be as eay as 123
-
+            We are a platform that enebles your child to learn 
+            at the comfort of your home without disrupting their environmrnt. <br />
+            
         </p><br />
         <p>
-        We are a platform that enebles the groeth of your 
-            child without chash hriwod hgoenwp wue ur tt ei e i <br />
-            hfirfiekedncrur erhfekjnfkjnej ei i beiiev thwt it can be as eay as 123
+            <br />
+        We target families that have toddlers , pre-school children
+         and junior primary children whose minds are maliable and eager to<br />
+        learn and absorb knowledge.            
+        </p>
+        <br />
+        <p>
+            <br />
+        We take pride in our holiday programs that make use of the free
+        time available on the holidays to teach invaluable skills to your children.<br />
+        These skills build confidence and give your child a sense of pride after they can <br/>
+        see tangiblr proof of their hard work and efforts.
             
         </p>
         <br />
         <p>
-        We are a platform that enebles the groeth of your 
-            child without chash hriwod hgoenwp wue ur tt ei e i <br />
-            hfirfiekedncrur erhfekjnfkjnej ei i beiiev thwt it can be as eay as 123
-            
-        </p>
         <br />
-        <p>
-        We are a platform that enebles the groeth of your 
-            child without chash hriwod hgoenwp wue ur tt ei e i <br />
-            hfirfiekedncrur erhfekjnfkjnej ei i beiiev thwt it can be as eay as 123
+        As the old saying goes  " education is the key to success " but here at<br />
+         Curious Minds 
+        we believe character creates opportunity. <br />
+        <br />
+        So why not build strong Character in your Child Early.
             
         </p>
     </div>
